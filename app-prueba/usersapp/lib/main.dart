dcaa104c976e4e9f48866aafd05fbc40f1e787c6
Axel Jovanny Quintero Trujillo <axel.jovanny.aj@gmail.com>
2020-01-23 13:58:56 -0600

    TabBar
    se agrega tabbar para separar widgets de crear y consultar

--- app-prueba/usersapp/lib/main.dart
+++ app-prueba/usersapp/lib/main.dart
@@ -29,7 +29,7 @@ class _HomePageState extends State<HomePage> {
       print("------JSON: >");
       print(usersData);
     });
-    
+
     // debugPrint(usersData.toString());
     debugPrint(usersData.length.toString());
   }
@@ -42,44 +42,59 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('User List'),
-        backgroundColor: Colors.indigo[900],
-      ),
-      body: ListView.builder(
-        itemCount: usersData == null ? 0 : usersData.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Card(
-            child: Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Row(
-                children: <Widget>[
-                  // Text("${usersData[index]["_id"]} - "),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      "$index",
-                      style:
-                          TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500),
-                    ),
-                  ),
-                  //CircleAvatar(
-                    //  backgroundImage:
-                      //    NetworkImage(usersData[index]['avatar'])),
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Text(
-                      "${usersData[index]["usuario"]} ${usersData[index]["nombre"]}",
-                      style: TextStyle(
-                          fontSize: 20.0, fontWeight: FontWeight.w700),
-                    ),
-                  ),
-                ],
-              ),
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.search)),
+                Tab(icon: Icon(Icons.create)),
+              ],
             ),
-          );
-        },
+            title: Text('Tabs Demo'),
+          ),
+          body: TabBarView(
+            children: [
+              ListView.builder(
+                itemCount: usersData == null ? 0 : usersData.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Row(
+                        children: <Widget>[
+                          // Text("${usersData[index]["_id"]} - "),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              "$index",
+                              style: TextStyle(
+                                  fontSize: 20.0, fontWeight: FontWeight.w500),
+                            ),
+                          ),
+                          CircleAvatar(
+                              backgroundImage:
+                                  NetworkImage(usersData[index]['avatar'])),
+                          Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Text(
+                              "${usersData[index]["usuario"]} ${usersData[index]["nombre"]}",
+                              style: TextStyle(
+                                  fontSize: 20.0, fontWeight: FontWeight.w700),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+              Icon(Icons.directions_transit),
+            ],
+          ),
+        ),
       ),
     );
   }
