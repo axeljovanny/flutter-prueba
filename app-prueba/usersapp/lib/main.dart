73218d2e2a97f37468c0c0fc7dbc0eafa507f958
Axel Jovanny Quintero Trujillo <axel.jovanny.aj@gmail.com>
2020-01-24 17:55:19 -0600

    SearchBar
    funciona el search, no funciona el scroll

--- app-prueba/usersapp/lib/main.dart
+++ app-prueba/usersapp/lib/main.dart
@@ -4,6 +4,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:fluttertoast/fluttertoast.dart';
 
+//MAIN
 void main() {
   runApp(
     MaterialApp(
@@ -23,16 +24,16 @@ class _HomePageState extends State<HomePage> {
   List usersData;
 
   /*** ----------------------------- ***/
-  String _nombre, _usuario;
+  String _nombre, _usuario, _search;
+
   // Crea un controlador de texto para recuperar el valor actual
   final myController = TextEditingController();
   final myController2 = TextEditingController();
-
+  final searchController = TextEditingController();
 
   Future getUsers() async {
-    http.Response response = await http.get('http://192.168.0.5:8080/api/user');
-    // debugPrint(response.body);
-    //data = json.decode(response.body);
+    http.Response response =
+        await http.get('http://192.168.0.38:8080/api/clientes');
     setState(() {
       usersData = json.decode(response.body);
       print("------JSON: >");
@@ -43,45 +44,31 @@ class _HomePageState extends State<HomePage> {
     debugPrint(usersData.length.toString());
   }
 
-  /*** ----------------------------- ***/
-  /**
-   * metodo para que se permite crear un json y mandar
-   * los datos a la base
-   */
-
-  Future<http.Response> postRequest () async {
+  Future<http.Response> postRequest() async {
     print("entrando");
     //tomar los datos tecleados por el usuario
     String user = myController2.text;
     String name = myController.text;
 
-    var url ='http://192.168.0.5:8080/api/user/create';
+    var url = 'http://192.168.0.38:8080/api/clientes';
 
-    Map data = {
-      'nombre':name,
-      'usuario':user,
-      'avatar':'deloitte'
-    };
+    Map data = {'nombre': name, 'usuario': user, 'avatar': 'deloitte'};
 
     //encode Map to JSON
     var body = json.encode(data);
 
     var response = await http.post(url,
-        headers: {"Content-Type": "application/json"},
-        body: body
-    );
+        headers: {"Content-Type": "application/json"}, body: body);
 
-    if(response.statusCode == 200){
+    if (response.statusCode == 200) {
       Fluttertoast.showToast(
           msg: "Registro guardado",
           toastLength: Toast.LENGTH_SHORT,
           gravity: ToastGravity.BOTTOM,
           backgroundColor: Colors.red,
           textColor: Colors.white,
-          fontSize: 16.0
-      );
-
-    }else{
+          fontSize: 16.0);
+    } else {
       Fluttertoast.showToast(
           msg: "Operacion fallida",
           toastLength: Toast.LENGTH_SHORT,
@@ -89,12 +76,26 @@ class _HomePageState extends State<HomePage> {
           timeInSecForIos: 1,
           backgroundColor: Colors.red,
           textColor: Colors.white,
-          fontSize: 16.0
-      );
+          fontSize: 16.0);
     }
     print("${response.statusCode}");
     print("${response.body}");
     return response;
+  }
+
+  void getSearch() async {
+    print("entrando");
+    //tomar los datos tecleados por el usuario
+    String search = searchController.text;
+    var url = 'http://192.168.0.38:8080/api/clientes/';
+
+    http.Response response = await http.get(url + search);
+
+    setState(() {
+      usersData = [json.decode(response.body)];
+    });
+    print("${response.statusCode}");
+    print("${response.body}");
   }
 
   @override
@@ -127,110 +128,8 @@ class _HomePageState extends State<HomePage> {
           ),
           body: TabBarView(
             children: [
-              SingleChildScrollView(
-                child: Column(
-                  children: <Widget>[
-                    Container(
-                      child: _Form(context),
-                    ),
-                    ListView.builder(
-                      scrollDirection: Axis.vertical,
-                      shrinkWrap: true,
-                      itemCount: usersData == null ? 0 : usersData.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Card(
-                          child: Padding(
-                              padding: const EdgeInsets.all(12.0),
-                              child: Column(
-                                children: <Widget>[
-                                  Container(
-                                    child: Row(
-                                      children: <Widget>[
-                                        // Text("${usersData[index]["_id"]} - "),
-                                        Padding(
-                                          padding: const EdgeInsets.all(8.0),
-                                          child: Text(
-                                            "$index",
-                                            style: TextStyle(
-                                                fontSize: 20.0,
-                                                fontWeight: FontWeight.w500),
-                                          ),
-                                        ),
-                                        // CircleAvatar(
-                                        // backgroundImage:
-                                        // NetworkImage(usersData[index]['avatar'])),
-                                        Padding(
-                                          padding: const EdgeInsets.all(10.0),
-                                          child: Text(
-                                            "${usersData[index]["usuario"]} ${usersData[index]["nombre"]}",
-                                            style: TextStyle(
-                                                fontSize: 20.0,
-                                                fontWeight: FontWeight.w700),
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ],
-                              )),
-                        );
-                      },
-                    ),
-                  ],
-                ),
-              ),
-              // widget de formulario de user
-              Form(
-
-                key: formKey,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Padding(padding: const EdgeInsets.all(10.0)),
-
-                    TextFormField(
-                      style: TextStyle(
-                          fontSize: 20.0, fontWeight: FontWeight.w700),
-                      // Recupera el texto que el usuario ha digitado utilizando nuestro
-                      controller: myController,
-                      decoration: InputDecoration(
-                          labelText: 'Nombre'
-                      ),
-                      validator: (input) => input.isEmpty ? ' Es obligatorio el Nombre' : null,
-                      onSaved: (input) => _nombre = input,
-                    ),
-                    SizedBox(height:10),
-                    Padding(padding: const EdgeInsets.all(10.0)),
-
-                    TextFormField(
-                      style: TextStyle(
-                          fontSize: 20.0, fontWeight: FontWeight.w700),
-                      // Recupera el texto que el usuario ha digitado utilizando nuestro
-                      controller: myController2,
-                      decoration: InputDecoration(
-                        labelText: 'Usuario',
-                      ),
-                      validator: (input) => input.isEmpty ? ' Es obligario el usuario': null,
-                      onSaved: (input) => _usuario = input,
-                    ),
-                    SizedBox(height:20),
-                    Row(
-
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: RaisedButton(
-                              //color: Colors.red,
-                              onPressed: _submit,
-                              child: Text('Registrate'),
-                            )
-                        )
-                      ],
-                    )
-                  ],
-                ),
-              )
+              _Form(context),
+              _Create(context),
             ],
           ),
         ),
@@ -240,45 +139,145 @@ class _HomePageState extends State<HomePage> {
 
   final _formKey = GlobalKey<FormState>();
 
-  @override
-  Widget _Form(BuildContext context) {
+  Widget _Create(BuildContext context) {
     return Form(
-      key: _formKey,
+      key: formKey,
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
         children: <Widget>[
+          Padding(padding: const EdgeInsets.all(10.0)),
           TextFormField(
-            decoration: const InputDecoration(
-              hintText: 'Search',
-            ),
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Please enter some text';
-              }
-              return value;
-            },
+            style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w700),
+            // Recupera el texto que el usuario ha digitado utilizando nuestro
+            controller: myController,
+            decoration: InputDecoration(labelText: 'Nombre'),
+            validator: (input) =>
+                input.isEmpty ? ' Es obligatorio el Nombre' : null,
+            onSaved: (input) => _nombre = input,
           ),
-          Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: Center(
-                child: RaisedButton(
-                  onPressed: () {
-                    if (_formKey.currentState.validate()) {}
-                  },
-                  child: Text('Search'),
-                ),
-              )),
+          SizedBox(height: 10),
+          Padding(padding: const EdgeInsets.all(10.0)),
+          TextFormField(
+            style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w700),
+            // Recupera el texto que el usuario ha digitado utilizando nuestro
+            controller: myController2,
+            decoration: InputDecoration(
+              labelText: 'Usuario',
+            ),
+            validator: (input) =>
+                input.isEmpty ? ' Es obligario el usuario' : null,
+            onSaved: (input) => _usuario = input,
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: RaisedButton(
+                    //color: Colors.red,
+                    onPressed: _submit,
+                    child: Text('Registrate'),
+                  ))
+            ],
+          )
         ],
       ),
     );
   }
 
+  @override
+  Widget _Lista(BuildContext context) {
+    return ListView.builder(
+      scrollDirection: Axis.vertical,
+      shrinkWrap: true,
+      itemCount: usersData == null ? 0 : usersData.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Card(
+          child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    child: Row(
+                      children: <Widget>[
+                        // Text("${usersData[index]["_id"]} - "),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            "$index",
+                            style: TextStyle(
+                                fontSize: 20.0, fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        // CircleAvatar(
+                        // backgroundImage:
+                        // NetworkImage(usersData[index]['avatar'])),
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Text(
+                            "${usersData[index]["usuario"]} ${usersData[index]["nombre"]}",
+                            style: TextStyle(
+                                fontSize: 20.0, fontWeight: FontWeight.w700),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              )),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget _Form(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Search',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+                controller: searchController,
+                onSaved: (input) => _search = input,
+              ),
+              Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: Center(
+                    child: RaisedButton(
+                      onPressed: _buscar,
+                      child: Text('Search'),
+                    ),
+                  )),
+            ],
+          ),
+        ),
+        _Lista(context)
+      ],
+    );
+  }
 
   //vallidacion y evento del button
   void _submit() {
-    if(formKey.currentState.validate()) {
+    if (formKey.currentState.validate()) {
       postRequest();
     }
+  }
 
+  //vallidacion y evento del button
+  void _buscar() {
+    getSearch();
   }
 }
